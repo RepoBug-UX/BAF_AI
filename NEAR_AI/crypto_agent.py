59e30f020060a32112b7fd2453ba9a06796cc087
Gabriel <ginsberg.gabriel@gmail.com>
2025-02-27 21:16:11 -0700

    fix: sorted nearAI agent

--- NEAR_AI/crypto_agent.py
+++ NEAR_AI/crypto_agent.py
@@ -14,7 +14,7 @@ def get_crypto_metadata():
     headers = {
         "Accepts": "application/json",
         "X-CMC_PRO_API_KEY": api_key
-    }
+    } 
     
     try:
         response = requests.get(url, headers=headers)
